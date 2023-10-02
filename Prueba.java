e0c49efe45de2b720141888017dd6f225ed07555
Nicolas <nicoaldopaez@gmail.com>
2023-10-02 09:21:12 +0200

    Ahora muestra mi grupo musical favorito

--- Prueba.java
+++ Prueba.java
@@ -1,7 +1,7 @@
 public class Prueba{
 	public static void main(String args[]){
-		System.out.println("Páez Amarilla");
 		System.out.println("29 13-02-1994");
 		System.out.println("Pastel");
+		System.out.println("Metallica");
 	}
 }
